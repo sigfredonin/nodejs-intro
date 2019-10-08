3eb5ce11ffd52cff9b830f3cc196c681e0f66a90
Sig Nin <signin@email.com>
2019-10-08 15:44:57 -0700

    Initialize nodejs tutorial repository

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello, world! -- dom-manipulation");
+
+function addListItem(input, target) {
+  let listItem = document.createElement('li');
+  listItem.innerHTML = input;
+  target.appendChild(listItem);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  let displayPromptButton = document.getElementById('displayPromptButton');
+  let viewInputColor = document.getElementById('color');
+  let viewColorList = document.getElementById('colorList');
+  displayPromptButton.addEventListener('click', function() {
+    console.log('clicked!');
+    let input = prompt('What is your favorite color?');
+    console.log('input is ', input);
+    viewInputColor.innerHTML = "Your favorite color is: " + input;
+    addListItem(input, viewColorList)
+  });
+});
