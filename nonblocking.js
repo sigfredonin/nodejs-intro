1a73a26181f46cd34861fe9415fa5c25b9bfd717
Sig Nin <signin@email.com>
2019-10-08 16:58:16 -0700

    blocking and non-blocking examples

---
+++ nonblocking.js
@@ -0,0 +1,10 @@
+console.log("blocking code example");
+
+var fs = require("fs");
+
+fs.readFile("input.txt", function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+
+console.log("blocking example ended");
